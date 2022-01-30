6ec9926430db7da6644f0ab7624702620edec8aa
QassemNa <alnasserqassem@gmail.com>
2022-01-30 17:16:17 +0300

    Fix header

---
+++ update_reports/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': "Update invoice report",
+    'category': "report",
+    'summary': 'Update Body and Header of invoice',
+    'depends': ["base", "account_accountant", "sale_management"],
+    'data': [
+        'report/Header_Update.xml'
+    ],
+    'installable': True,
+    'demo': [],
+    'application':False,
+    'auto_install': False
+}
